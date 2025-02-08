3bceaaf98007aa8d73ab1eb2b87a504db09137e8
MrFragg <Mrfraggsiu@gmail.com>
2025-02-08 17:21:39 +0700

    hehehehe

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(function () {
-  const noBtn = $(".btn.no");
-  handleNoButton(noBtn);
+  handleNoButton();
+  handleYesButton();
+  handleEnvelope();
 });
 
 function randomRange(min, max) {
@@ -14,7 +15,9 @@ function randomMove(noBtn) {
   noBtn.offset({ left: x, top: y });
 }
 
-function handleNoButton(noBtn) {
+function handleNoButton() {
+  const noBtn = $(".btn.no");
+
   noBtn
     .on("mouseenter", function () {
       randomMove(noBtn);
@@ -22,4 +25,34 @@ function handleNoButton(noBtn) {
     .on("click", function () {
       randomMove(noBtn);
     });
+}
+
+function handleYesButton() {
+  $(".btn.yes").on("click", function () {
+    $(".group-btn").removeClass("active");
+    $(".form").addClass("active");
+    $(".main-title").text("Vui lòng cung cấp thông tin")
+  });
+}
+
+function handleEnvelope() {
+  let isOpen = false;
+  const popup = $("#popup");
+  const formName = $("input#form-name");
+  const username = $(".username");
+
+  $(".close-btn").on("click", function () {
+    if (isOpen) {
+      popup.removeClass("active");
+      isOpen = false;
+    }
+  });
+
+  $(".form-submit").on("click", function () {
+    if (!isOpen && String(formName.val()).length > 0) {
+      popup.addClass("active");
+      username.text((formName.val()));
+      isOpen = true;
+    }
+  });
 }
